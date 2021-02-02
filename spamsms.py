bead86444efda48f8b4290dc2aaffc1f83ee0afe
Aryaman <59313789+Aryaman-Das@users.noreply.github.com>
2021-02-02 17:52:39 +0530

    improve looks

--- spamsms.py
+++ spamsms.py
@@ -12,13 +12,11 @@ def spamsmsdetection(data):
     y = df['v1']
     le = LabelEncoder()
     y = le.fit_transform(y)
-
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=7, test_size=0.2)
     c = CountVectorizer(ngram_range=(1,2))
     x_train = c.fit_transform(x_train)
     l = LogisticRegression(max_iter = 10000)
     l.fit(x_train, y_train)
-
     prediction = l.predict(c.transform(x_test))
     a = accuracy_score(y_test, prediction)
 
